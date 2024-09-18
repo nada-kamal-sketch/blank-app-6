27f574c52dd2563c8be8c525e7c4dca428be310b
nada-kamal-sketch <nhega854@gmail.com>
2024-09-18 20:23:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
-import streamlit as st
+'''import streamlit as st
+st.sidebar.title('sidbar')
 st.header('calculate area')
+with sidebar:
 choose=st.selectbox('choose the shape',['circle','rectangle'])
 if choose=='circle':
   r=st.number_input('enter the radius',min_value=1,max_value=100)
@@ -10,4 +12,10 @@ elif choose=='rectangle':
   area=l*b
 btn=st.button('calculate')
 if btn:
-  st.write(f'area={area}')
+  st.write(f'area={area}')'''
+import pandas as pd
+import streamlit as st
+st.header('file upload app2')
+file=st.file_uploader('upload dataset',type=['csv'])
+df=pd.read_csv(file)
+st.write(df)
