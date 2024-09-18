74e9f7752ed27bee6585ec341fa0a2e902539d28
nada-kamal-sketch <nhega854@gmail.com>
2024-09-18 20:10:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.header('calculate area')
+choose=st.selectbox('choose the shape',['circle','rectangle'])
+if choose=='circle':
+  r=st.number_input('enter the radius',min_value=1,max_value=100)
+  area=3.14*r*r
+elif choose=='rectangle':
+  l=st.number_input('enter the length',min_value=1,max_value=100)
+  b=st.number_input('enter the breadth',min_value=1,max_value=100)
+  area=l*b
+btn=st.button('calculate')
+if btn:
+  st.write(f'area={area}')
